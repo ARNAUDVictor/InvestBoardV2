93db7d2099a8660f533275cf4816a7ef5ca6954a
ARNAUDVictor <142483758+ARNAUDVictor@users.noreply.github.com>
2024-09-05 22:22:38 +0200

    add new stats

--- src/components/GlobalStatsView.svelte
+++ src/components/GlobalStatsView.svelte
@@ -3,8 +3,11 @@
 
 </script>
 <p>Nombre total de projet : {$statsStore.totalProjectAmount}</p>
+<p>Nombre de projet terminés : {$statsStore.numberCompletedProjects} soit {$statsStore.percentageOfCompletedProjects} %</p>
 <p>Argent total : {$statsStore.totalLoanedMoney} €</p>
-<p>Argent total : {$statsStore.totalCurrentlyLoanedMoney} €</p>
+<p>Argent actuellement investie : {$statsStore.totalCurrentlyLoanedMoney} €</p>
+<p>Total intérets reçus : {$statsStore.totalInterest} €</p>
+
 
 <style>
 
--- src/store/statsStore.js
+++ src/store/statsStore.js
@@ -21,8 +21,7 @@ function getTotalLoanedMoney(transactions){
 
 function getTotalCurrentlyLoanedMoney(transactions){
     if(Object.keys(transactions).length > 0){
-        console.log(transactions.projects)
-        let projects = transactions.projects.filter((transaction) => transaction["Statut"] == "Prêt en cours");
+        let projects = transactions.projects.filter((project) => project["Statut"] == "Prêt en cours");
         let money = 0;
         for(let project of projects){
             money += Number(project.Montant);
@@ -32,13 +31,47 @@ function getTotalCurrentlyLoanedMoney(transactions){
     return 0;
 }
 
+function getNumberCompletedProjects(transaction){
+    if(Object.keys(transaction).length > 0){
+        let projects = transaction.projects.filter((project) => project["Statut"] == "Prêt remboursé");
+        return projects.length;
+    }
+    return 0;
+}
+
+function getTotalInterests(transactions){
+    if(Object.keys(transactions).length > 0){
+        let money = 0;
+        for(let remboursement of transactions.remboursements){
+            let remb = remboursement["Intérêts remboursés"];
+            if(typeof remb === "string"){
+                remb = remb.replace(",", ".");
+            }
+            console.log(remb);
+            money += Number(remb);
+        }
+        return money;
+    }
+    return 0;
+}
+
 function createStatsStore() {
-        //création du store derivé du store "transactionStore"
+    // Création du store dérivé du store "transactionsStore"
     const store = derived(transactionsStore, $transactionsStore => {
+        const totalProjectAmount = getNumberProject($transactionsStore) || 0;
+        const totalLoanedMoney = getTotalLoanedMoney($transactionsStore) || 0;
+        const totalCurrentlyLoanedMoney = getTotalCurrentlyLoanedMoney($transactionsStore) || 0;
+        const numberCompletedProjects = getNumberCompletedProjects($transactionsStore) || 0;
+        const percentageOfCompletedProjects = totalProjectAmount > 0 ? Math.round((numberCompletedProjects / totalProjectAmount) * 100) : 0;
+        const totalInterest = getTotalInterests($transactionsStore) || 0;
+
         return {
-            totalProjectAmount: getNumberProject($transactionsStore),
-            totalLoanedMoney: getTotalLoanedMoney($transactionsStore),
-            totalCurrentlyLoanedMoney: getTotalCurrentlyLoanedMoney($transactionsStore),
+            totalProjectAmount,
+            totalLoanedMoney,
+            totalCurrentlyLoanedMoney,
+            numberCompletedProjects,
+            percentageOfCompletedProjects,
+            totalInterest,
         };
     });
 
